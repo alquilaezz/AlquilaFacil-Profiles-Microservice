cbf69580a0a87d576711010d0a4a4e8f8490455d
JCarbajal22 <u202121881@upc.edu.pe>
2025-12-06 10:54:47 -0500

    CORS

--- app/main.py
+++ app/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .database import Base, engine
 from .routers import profiles
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Profiles Service")
+
+origins = [
+    "http://localhost:5173",                # para desarrollo local
+    "https://alquilaezz.netlify.app",       # tu front en producción
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,          # o ["*"] si quieres permitir todos (no recomendado para prod)
+    allow_credentials=True,
+    allow_methods=["*"],            # GET, POST, PUT, DELETE, etc.
+    allow_headers=["*"],            # "Content-Type", "Authorization", etc.
+)
 
 app.include_router(profiles.router)
